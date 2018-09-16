06d33edbf4d174766a2a186254a6167b7e9ddfc8
TABelousova <garitova.92@mail.ru>
2018-09-16 21:22:28 +0300

    добавляет вывод результатов из таблицы

--- records.php
+++ records.php
@@ -2,22 +2,28 @@
 <head>
 </head>
 <body>
-<table border="1px">
-<tr>
-    <th>Имя игрока</th>
-    <th>Время</th>
-    <th>Размер поля</th>
-    <th>Количество мин</th>
-</tr>
-</table>
 <?php
 require_once 'connection.php';
 $link = mysqli_connect($host, $user, $password, $database)
 or die("Ошибка " . mysqli_error($link));
-$query = "SELECT 'name', 'game_time', 'field_size', 'mines'  FROM records";
+$query = "SELECT name, game_time, field_size, mines FROM records";
 $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
-if ($result) {
-    echo "Выполнение запроса прошло успешно";
+if($result)
+{
+    $rows = mysqli_num_rows($result); // количество полученных строк
+     
+    echo "<table><tr><th>Игрок</th><th>Время</th><th>Размер поля</th><th>Количество мин</th></tr>";
+    for ($i = 0 ; $i < $rows ; ++$i)
+    {
+        $row = mysqli_fetch_row($result);
+        echo "<tr>";
+            for ($j = 0 ; $j < 4 ; ++$j) echo "<td>$row[$j]</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+     
+    // очищаем результат
+    mysqli_free_result($result);
 }
 ?>
 </body>
