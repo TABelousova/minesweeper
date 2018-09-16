0d325ecac159c86492a12821623c9ec7ec679a52
TABelousova <garitova.92@mail.ru>
2018-09-16 14:47:32 +0300

    создает шапку для таблицы результатов

--- records.php
+++ records.php
@@ -1,9 +1,24 @@
+<html>
+<head>
+</head>
+<body>
+<table border="1px">
+<tr>
+    <th>Имя игрока</th>
+    <th>Время</th>
+    <th>Размер поля</th>
+    <th>Количество мин</th>
+</tr>
+</table>
 <?php
 require_once 'connection.php';
 $link = mysqli_connect($host, $user, $password, $database)
 or die("Ошибка " . mysqli_error($link));
-$query = "SELECT * FROM records";
+$query = "SELECT 'name', 'game_time', 'field_size', 'mines'  FROM records";
 $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
 if ($result) {
     echo "Выполнение запроса прошло успешно";
 }
+?>
+</body>
+</html>
