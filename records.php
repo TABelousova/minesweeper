ab8aa37ce09d6e30e9ee6cd6da70bc2ca9956d20
TABelousova <garitova.92@mail.ru>
2018-09-19 23:58:11 +0300

    подключает файл стилей к таблице рекордов и сбрасывает стиль таблицы

--- records.php
+++ records.php
@@ -1,7 +1,10 @@
 <html>
 <head>
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <table class="result_field">
+    <tr><th>Игрок</th><th>Время</th><th>Размер поля</th><th>Количество мин</th></tr>
 <?php
 require_once 'connection.php';
 $link = mysqli_connect($host, $user, $password, $database)
@@ -11,8 +14,6 @@ $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($lin
 if($result)
 {
     $rows = mysqli_num_rows($result); // количество полученных строк
-     
-    echo "<table><tr><th>Игрок</th><th>Время</th><th>Размер поля</th><th>Количество мин</th></tr>";
     for ($i = 0 ; $i < $rows ; ++$i)
     {
         $row = mysqli_fetch_row($result);
@@ -20,11 +21,11 @@ if($result)
             for ($j = 0 ; $j < 4 ; ++$j) echo "<td>$row[$j]</td>";
         echo "</tr>";
     }
-    echo "</table>";
      
     // очищаем результат
     mysqli_free_result($result);
 }
 ?>
+</table>
 </body>
 </html>
